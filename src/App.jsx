1062d5a6dcc6559b0b938724f5b5fc90ebf5fd9b
Akinbo Olaoluwa Daniel <big_dahn@dahn>
2025-01-09 11:47:28 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from "./components/NavBar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
-    const [isOpen,setIsOpen] =useState(true)
+    const [isOpen,setIsOpen] =useState(false)
     return (
         <div className="max-w-6xl my-3 mx-auto px-8 lg:px-20 ">
             {isOpen && <Sidebar setIsOpen={setIsOpen} isOpen={isOpen} />}
